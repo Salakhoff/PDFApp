31c89e4905781ea95ff9bf8b584fa56d10f16616
Salakhoff <salakhoff.ilfat@gmail.com>
2025-11-15 16:19:54 +0300

    Initial Commit

---
+++ MyPDFApp/MyPDFAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPDFAppApp.swift
+//  MyPDFApp
+//
+//  Created by Ильфат Салахов on 15.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyPDFAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
